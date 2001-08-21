5fa917dc5ec6f613b5156f26d6e9876f5fbc4d79
Administrator <administrator@chrisoldwood.com>
2001-08-21 10:05:16 +0000

    Changed to use new template TPtrArray class.

--- StrArray.hpp
+++ StrArray.hpp
@@ -19,7 +19,7 @@
 *******************************************************************************
 */
 
-class CStrArray : public CPtrArray
+class CStrArray : protected TPtrArray<CString>
 {
 public:
 	//
@@ -31,12 +31,17 @@ public:
 	//
 	// Methods.
 	//
-	CString& Item(int nIndex) const;
-	CString& operator[](int nIndex) const;
+	int Size() const;
 
+	const CString& At(int nIndex) const;
+	const CString& operator[](int nIndex) const;
+
+	void Set(int nIndex, const CString& rString);
 	int  Add(const CString& rString);
 	void Insert(int nIndex, const CString& rString);
-	void Remove(int nIndex);
+
+	void Delete(int nIndex);
+	void DeleteAll();
 
 	int Find(const char* pszString, bool bIgnoreCase = false) const;
 };
@@ -48,30 +53,66 @@ public:
 *******************************************************************************
 */
 
-inline CString& CStrArray::Item(int nIndex) const
+inline CStrArray::CStrArray()
 {
-	return *(static_cast<CString*>(CPtrArray::Item(nIndex)));
 }
 
-inline CString& CStrArray::operator[](int nIndex) const
+inline CStrArray::~CStrArray()
 {
-	return CStrArray::Item(nIndex);
+	DeleteAll();
+}
+
+inline int CStrArray::Size() const
+{
+	return TPtrArray<CString>::Size();
+}
+
+inline const CString& CStrArray::At(int nIndex) const
+{
+	return *(TPtrArray<CString>::At(nIndex));
+}
+
+inline const CString& CStrArray::operator[](int nIndex) const
+{
+	return *(TPtrArray<CString>::At(nIndex));
+}
+
+inline void CStrArray::Set(int nIndex, const CString& rString)
+{
+	*(TPtrArray<CString>::At(nIndex)) = rString;
 }
 
 inline int CStrArray::Add(const CString& rString)
 {
-	return CPtrArray::Add(new CString(rString));
+	return TPtrArray<CString>::Add(new CString(rString));
 }
 
 inline void CStrArray::Insert(int nIndex, const CString& rString)
 {
-	CPtrArray::Insert(nIndex, new CString(rString));
+	TPtrArray<CString>::Insert(nIndex, new CString(rString));
 }
 
-inline void CStrArray::Remove(int nIndex)
+inline void CStrArray::Delete(int nIndex)
 {
-	delete &Item(nIndex);
-	CPtrArray::Remove(nIndex);
+	TPtrArray<CString>::Delete(nIndex);
+}
+
+inline void CStrArray::DeleteAll()
+{
+	TPtrArray<CString>::DeleteAll();
+}
+
+inline int CStrArray::Find(const char* pszString, bool bIgnoreCase) const
+{
+	ASSERT(pszString != NULL);
+
+	for (int i = 0; i < Size(); i++)
+	{
+		if (At(i).Compare(pszString, bIgnoreCase) == 0)
+			return i;
+	}
+
+	return -1;
 }
 
 #endif //STRARRAY_HPP
