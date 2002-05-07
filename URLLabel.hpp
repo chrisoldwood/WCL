7c17067ed80aeb4f7ada00a026ea4d05b8620b27
Administrator <administrator@chrisoldwood.com>
2002-05-07 10:56:08 +0000

    Vss2Git

---
+++ URLLabel.hpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+** (C) Chris Oldwood
+**
+** MODULE:		URLLABEL.HPP
+** COMPONENT:	Windows C++ Library.
+** DESCRIPTION:	The CURLLabel class declaration.
+**
+*******************************************************************************
+*/
+
+// Check for previous inclusion
+#ifndef URLLABEL_HPP
+#define URLLABEL_HPP
+
+/******************************************************************************
+** 
+** A STATIC based control with the ability to behave like a hyperlink.
+**
+*******************************************************************************
+*/
+
+class CURLLabel : public CLabel
+{
+public:
+	//
+	// Constructors/Destructor.
+	//
+	CURLLabel();
+	~CURLLabel();
+	
+protected:
+	//
+	// Members.
+	//
+};
+
+/******************************************************************************
+**
+** Implementation of inline functions.
+**
+*******************************************************************************
+*/
+
+#endif //URLLABEL_HPP
