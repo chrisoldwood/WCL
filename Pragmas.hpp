f190854d193d975a363e47099b08e06a7ddb6a11
Administrator <administrator@chrisoldwood.com>
2003-05-15 15:42:01 +0000

    Ordered pragmas by number and removed those which can be fixed by code changes.

--- Pragmas.hpp
+++ Pragmas.hpp
@@ -12,53 +12,42 @@
 #ifndef PRAGMAS_HPP
 #define PRAGMAS_HPP
 
-// Creating/using precompiled header.
-#pragma warning ( disable : 4699 )
-
-// Unreferenced formal parameter.
-#pragma warning ( disable : 4100 )
-
-// Unreferenced local function has been removed.
-#pragma warning ( disable : 4505 )
-
-// Statement has no effect
-#pragma warning ( disable : 4705 )
-
-// Inline function not expanded.
-#pragma warning ( disable : 4710 )
-
-// Non-constant aggregate initializer.
-#pragma warning ( disable : 4204 )
-
-// Conditional expression is constant.
-// Caused by ASSERT(false).
-#pragma warning ( disable : 4127 )
-
 // Nameless struct/union.
 #pragma warning ( disable : 4201 )
-
-// Unreferenced inline function removed.
-#pragma warning ( disable : 4514 )
-
-// Forcing value to bool true or false.
-#pragma warning ( disable : 4800 )
-
-// No assignment operator could be generated.
-#pragma warning ( disable : 4512 )
-
-// Zero-sized array used in struct/union.
-#pragma warning ( disable : 4200 )
 
 // 'this' used in base member initialisation list.
 #pragma warning ( disable : 4355 )
 
+// Unreferenced local function has been removed.
+#pragma warning ( disable : 4505 )
+
 // Copy constructor could not be generated.
+// Caused by members which are references.
 #pragma warning ( disable : 4511 )
+
+// No assignment operator could be generated.
+// Caused by members which are references.
+#pragma warning ( disable : 4512 )
+
+// Unreferenced inline function removed.
+#pragma warning ( disable : 4514 )
+
+// Creating/using precompiled header.
+#pragma warning ( disable : 4699 )
+
+// Statement has no effect
+//#pragma warning ( disable : 4705 )
+
+// Inline function not expanded.
+#pragma warning ( disable : 4710 )
+
+// Forcing value to bool true or false.
+#pragma warning ( disable : 4800 )
 
 #ifdef _DEBUG
 
-// Unreachable code, caused by ASSERT macros
-// when compiling a release build with asserts.
+// Unreachable code.
+// Caused by ASSERT macros when compiling a release build with asserts.
 #pragma warning ( disable : 4702 )
 
 #endif
