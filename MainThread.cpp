4b0a41f15c4661b9577571656e87ea0b4b383a2d
Administrator <administrator@chrisoldwood.com>
2004-09-17 08:28:51 +0000

    Moved implementation to a .cpp file as it now depends on CApp.

--- MainThread.cpp
+++ MainThread.cpp
@@ -2,13 +2,18 @@
 ** (C) Chris Oldwood
 **
 ** MODULE:		MAINTHREAD.CPP
-** COMPONENT:	Windows C++ Library
+** COMPONENT:	Windows C++ Library.
 ** DESCRIPTION:	CMainThread class definition.
 **
 *******************************************************************************
 */
 
 #include "wcl.hpp"
+
+#ifdef _DEBUG
+// For memory leak detection.
+#define new DBGCRT_NEW
+#endif
 
 /******************************************************************************
 ** Method:		Constructor.
@@ -24,7 +29,7 @@
 
 CMainThread::CMainThread()
 {
-	m_hHandle = NULL;
+	m_hThread = NULL;
 	m_dwID    = ::GetCurrentThreadId();
 }
 
@@ -42,4 +47,40 @@ CMainThread::CMainThread()
 
 CMainThread::~CMainThread()
 {
+}
+
+/******************************************************************************
+** Method:		Run.
+**
+** Description:	The main processing loop for the thread.
+**
+** Parameters:	None.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CMainThread::Run()
+{
+	// Forward to base class.
+	CMsgThread::Run();
+}
+
+/******************************************************************************
+** Method:		OnThreadMsg()
+**
+** Description:	Message handler for general thread messages.
+**
+** Parameters:	Standard thread message parameters.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CMainThread::OnThreadMsg(UINT nMsg, WPARAM wParam, LPARAM lParam)
+{
+	// Forward to Application object.
+	CApp::This().OnThreadMsg(nMsg, wParam, lParam);
 }
