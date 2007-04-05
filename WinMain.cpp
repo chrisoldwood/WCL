91731f9058958ead0030afa53f713ebc8e9811bb
Admin <admin@chrisoldwood.com>
2007-04-05 20:25:28 +0000

    Moved TRACE logging code to a new TraceLogger class so it can be used in DLLs.

--- WinMain.cpp
+++ WinMain.cpp
@@ -17,72 +17,8 @@
 #define new DBGCRT_NEW
 #endif
 
-/******************************************************************************
-**
-** Local variables.
-**
-*******************************************************************************
-*/
-
-// Trace log file path.
-static char g_szTraceLog[MAX_PATH];
-
-// Did last message include CRLF?
-static bool g_bNewLine = true;				
-
-// Disable ASSERT dialogs?
-static bool g_bConsume = false;
-
-/******************************************************************************
-** Function: 	TraceLogger()
-**
-** Description:	The CRT hook function to write all TRACE and ASSERTs to a file.
-**
-** Parameters:	See _CrtSetReportHook.
-**
-** Returns:		See _CrtSetReportHook.
-**
-*******************************************************************************
-*/
-
-static int TraceLogger(int /*nType*/, char* pszMessage, int* piRetVal)
-{
-	// Create full logfile path.
-	if (g_szTraceLog[0] == '\0')
-		strcpy(g_szTraceLog, CPath(CPath::ApplicationDir(), "TraceLog.txt"));
-
-	FILE* fLogFile = fopen(g_szTraceLog, "a");
-
-	// Opened log file okay?
-	if (fLogFile != NULL)
-	{
-		// Start of new message?
-		if (g_bNewLine)
-		{
-			time_t	tTime  = time(NULL);
-			tm*		pTime  = localtime(&tTime);
-			char	szTime[50];
-
-			// Write the date and time.
-			strftime(szTime, sizeof(szTime), "[%d/%m/%y %H:%M:%S] ", pTime);
-			fputs(szTime, fLogFile);
-		}
-
-		// Write the message and close.
-		fputs(pszMessage, fLogFile);
-		fclose(fLogFile);
-
-		// Message ended with CRLF?
-		for (const char* psz = pszMessage; *psz != '\0'; psz++)
-			g_bNewLine = ((*psz == '\r') || (*psz == '\n'));
-	}
-
-	// Don't force a debug break.
-	*piRetVal = FALSE;
-
-	// Chain to _CrtDbgReport.
-	return g_bConsume;
-}
+// Using declarations.
+using namespace WCL;
 
 /******************************************************************************
 ** Function: 	WinMain()
@@ -104,8 +40,8 @@ extern "C" int WINAPI WinMain(HINSTANCE hCurrInst, HINSTANCE /*hPrevInst*/,
 								LPSTR lpszCmdLine, int iCmdShow)
 {
 #ifdef _DEBUG
-	// Set TRACE/ASSERT logging function.
-	_CrtSetReportHook(TraceLogger);
+	// Install TRACE/ASSERT logging function.
+	TraceLogger::Install();
 #endif
 
 	// Use the Windows locale.
