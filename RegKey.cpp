09b53d3ec5c87f15ee4212944d0439b8f24d0cc5
Admin <admin@chrisoldwood.com>
2007-05-10 22:27:53 +0000

    Renamed class and methods and reimplemented to throw exceptions.

--- RegKey.cpp
+++ RegKey.cpp
@@ -1,12 +1,7 @@
-/******************************************************************************
-** (C) Chris Oldwood
-**
-** MODULE:		REGKEY.CPP
-** COMPONENT:	Windows C++ Library.
-** DESCRIPTION:	CRegKey class definition.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//! \file   RegKey.cpp
+//! \brief  The RegKey class definition.
+//! \author Chris Oldwood
 
 #include "wcl.hpp"
 
@@ -15,77 +10,62 @@
 #define new DBGCRT_NEW
 #endif
 
-/******************************************************************************
-** Method:		Constructor.
-**
-** Description:	.
-**
-** Parameters:	None.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+// Using declarations.
+using WCL::RegistryException;
 
-CRegKey::CRegKey()
+namespace WCL
+{
+
+////////////////////////////////////////////////////////////////////////////////
+//! Default constructor.
+
+RegKey::RegKey()
 	: m_hKey(NULL)
 {
 }
 
-/******************************************************************************
-** Method:		Destructor.
-**
-** Description:	.
-**
-** Parameters:	None.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//! Destructor.
 
-CRegKey::~CRegKey()
+RegKey::~RegKey()
 {
 	Close();
 }
 
-/******************************************************************************
-** Method:		Open()
-**
-** Description:	Opens the specified key for reading.
-**
-** Parameters:	hKey		The root key.
-**				pszSubKey	The path to the subkey.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//! Create a key and open it for writing.
 
-bool CRegKey::Open(HKEY hKey, const char* pszSubKey)
+void RegKey::Create(HKEY hParentKey, const char* pszSubKey)
 {
-	ASSERT(m_hKey    == NULL);
-	ASSERT(hKey      != NULL);
-	ASSERT(pszSubKey != NULL);
+	ASSERT(m_hKey     == NULL);
+	ASSERT(hParentKey != NULL);
+	ASSERT(pszSubKey  != NULL);
 
-	LONG lResult = ::RegOpenKeyEx(hKey, pszSubKey, NULL, KEY_QUERY_VALUE, &m_hKey);
+	LONG lResult = ::RegCreateKey(hParentKey, pszSubKey, &m_hKey);
 
-	return (lResult == ERROR_SUCCESS);
+	if (lResult != ERROR_SUCCESS)
+		throw RegistryException(lResult, "Failed to create a registry key");
 }
 
-/******************************************************************************
-** Method:		Close()
-**
-** Description:	Close the key.
-**
-** Parameters:	None.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//! Open the key for reading or writing.
 
-void CRegKey::Close()
+void RegKey::Open(HKEY hParentKey, const char* pszSubKey, REGSAM dwAccess)
+{
+	ASSERT(m_hKey     == NULL);
+	ASSERT(hParentKey != NULL);
+	ASSERT(pszSubKey  != NULL);
+
+	LONG lResult = ::RegOpenKeyEx(hParentKey, pszSubKey, NULL, dwAccess, &m_hKey);
+
+	if (lResult != ERROR_SUCCESS)
+		throw RegistryException(lResult, "Failed to open a registry key");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Close the key.
+
+void RegKey::Close()
 {
 	// Key open?
 	if (m_hKey != NULL)
@@ -96,20 +76,42 @@ void CRegKey::Close()
 	}
 }
 
-/******************************************************************************
-** Method:		QueryString()
-**
-** Description:	Gets a string value.
-**
-** Parameters:	pszName		The name of the value.
-**				pszDefault	The default to return if not found/on error.
-**
-** Returns:		Nothing.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//! Read the default value for the key.
 
-CString CRegKey::QueryString(const char* pszName, const char* pszDefault) const
+CString RegKey::ReadDefaultValue() const
+{
+	ASSERT(m_hKey != NULL);
+	
+	LONG lSize = 0;
+
+	// Get the data type and size;
+	LONG lResult = ::RegQueryValue(m_hKey, NULL, NULL, &lSize);
+
+	if (lResult != ERROR_SUCCESS)
+		throw RegistryException(lResult, "Failed to read the default key value size");
+
+	// Allow for a nul terminator.
+	++lSize;
+
+	// Allocate a temporary buffer.
+	char* pszBuffer = (char*) _alloca(lSize);
+
+	memset(pszBuffer, 0, lSize);
+
+	// Fetch the data.
+	lResult = ::RegQueryValue(m_hKey, NULL, pszBuffer, &lSize);
+
+	if (lResult != ERROR_SUCCESS)
+		throw RegistryException(lResult, "Failed to read the default key value");
+
+	return pszBuffer;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Read a named string value under the key.
+
+CString RegKey::ReadStringValue(const char* pszName, const char* pszDefault) const
 {
 	ASSERT(m_hKey     != NULL);
 	ASSERT(pszName    != NULL);
@@ -137,4 +139,132 @@ CString CRegKey::QueryString(const char* pszName, const char* pszDefault) const
 		return pszDefault;
 
 	return pszBuffer;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Write the default value for the key.
+
+void RegKey::WriteDefaultValue(const char* pszValue)
+{
+	ASSERT(m_hKey   != NULL);
+	ASSERT(pszValue != NULL);
+
+	LONG lResult = ::RegSetValue(m_hKey, NULL, REG_SZ, pszValue, strlen(pszValue));
+
+	if (lResult != ERROR_SUCCESS)
+		throw RegistryException(lResult, "Failed to write the default key value");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Write a named string value under the key.
+
+void RegKey::WriteStringValue(const char* pszName, const char* pszValue)
+{
+	ASSERT(m_hKey   != NULL);
+	ASSERT(pszValue != NULL);
+
+	LONG lResult = ::RegSetValueEx(m_hKey, pszName, 0, REG_SZ, reinterpret_cast<CONST BYTE*>(pszValue), strlen(pszValue)+1);
+
+	if (lResult != ERROR_SUCCESS)
+		throw RegistryException(lResult, "Failed to write a regsitry value");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Query if the key exists.
+
+bool RegKey::Exists(HKEY hParentKey, const char* pszSubKey)
+{
+	ASSERT(hParentKey != NULL);
+	ASSERT(pszSubKey  != NULL);
+
+	HKEY hKey = NULL;
+
+	// Try and open the key for reading.
+	LONG lResult = ::RegOpenKeyEx(hParentKey, pszSubKey, NULL, KEY_READ, &hKey);
+
+	if (hKey != NULL)
+		::RegCloseKey(hKey);
+
+	return (lResult == ERROR_SUCCESS);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Delete the key. This only deletes the key and any named values, it does not
+//! recursively delete any subkeys.
+//! If the method fails the error code can be retrieved with GetLastError().
+
+bool RegKey::Delete(HKEY hParentKey, const char* pszSubKey)
+{
+	ASSERT(hParentKey != NULL);
+	ASSERT(pszSubKey  != NULL);
+
+	LONG lResult = ::RegDeleteKey(hParentKey, pszSubKey);
+
+	// Fill in LastError() for the caller.
+	if (lResult != ERROR_SUCCESS)
+		::SetLastError(lResult);
+
+	return (lResult == ERROR_SUCCESS);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Read the default value for a key.
+
+CString RegKey::ReadKeyDefaultValue(HKEY hParentKey, const char* pszSubKey)
+{
+	ASSERT(hParentKey != NULL);
+	ASSERT(pszSubKey  != NULL);
+
+	RegKey oKey;
+
+	oKey.Open(hParentKey, pszSubKey, KEY_READ);
+	
+	return oKey.ReadDefaultValue();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Write the default value for a key. The key is created if it doesn't exist.
+
+void RegKey::WriteKeyDefaultValue(HKEY hParentKey, const char* pszSubKey, const char* pszValue)
+{
+	ASSERT(hParentKey != NULL);
+	ASSERT(pszSubKey  != NULL);
+	ASSERT(pszValue   != NULL);
+
+	RegKey oKey;
+
+	// Open key, creating if necessary.
+	if (Exists(hParentKey, pszSubKey))
+		oKey.Open(hParentKey, pszSubKey, KEY_WRITE);
+	else
+		oKey.Create(hParentKey, pszSubKey);
+
+	// Set the value.
+	oKey.WriteDefaultValue(pszValue);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Write a named string value under a key. The key is created if it doesn't
+//! exist.
+
+void RegKey::WriteKeyStringValue(HKEY hParentKey, const char* pszSubKey, const char* pszName, const char* pszValue)
+{
+	ASSERT(hParentKey != NULL);
+	ASSERT(pszSubKey  != NULL);
+	ASSERT(pszName    != NULL);
+	ASSERT(pszValue   != NULL);
+
+	RegKey oKey;
+
+	// Open key, creating if necessary.
+	if (Exists(hParentKey, pszSubKey))
+		oKey.Open(hParentKey, pszSubKey, KEY_WRITE);
+	else
+		oKey.Create(hParentKey, pszSubKey);
+
+	// Set the value.
+	oKey.WriteStringValue(pszName, pszValue);
+}
+
+//namespace WCL
 }
--- RegKey.hpp
+++ RegKey.hpp
@@ -1,63 +1,114 @@
-/******************************************************************************
-** (C) Chris Oldwood
-**
-** MODULE:		REGKEY.HPP
-** COMPONENT:	Windows C++ Library.
-** DESCRIPTION:	The CRegKey class declaration.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//! \file   RegKey.hpp
+//! \brief  The RegKey class declaration.
+//! \author Chris Oldwood
 
 // Check for previous inclusion
 #ifndef REGKEY_HPP
 #define REGKEY_HPP
 
-/******************************************************************************
-** 
-** This class is used to access the registry.
-**
-*******************************************************************************
-*/
+namespace WCL
+{
 
-class CRegKey
+////////////////////////////////////////////////////////////////////////////////
+//! The class used to read and write from the loca Registry.
+
+class RegKey
 {
 public:
-	//
-	// Constructors/Destructor.
-	//
-	CRegKey();
-	~CRegKey();
+	//! Default constructor.
+	RegKey();
+
+	//! Destructor.
+	~RegKey();
 	
 	//
-	// Accessors.
+	// Properties.
 	//
+
+	//! Get the underlying handle.
 	HKEY Handle() const;
+
+	//! Query if the key is open.
+	bool IsOpen() const;
 
 	//
 	// Methods.
 	//
-	bool Open(HKEY hKey, const char* pszSubKey);
+
+	//! Create a key and open it for writing.
+	void Create(HKEY hParentKey, const char* pszSubKey); // throw(RegistryException)
+
+	//! Open the key for reading or writing.
+	void Open(HKEY hParentKey, const char* pszSubKey, REGSAM dwAccess); // throw(RegistryException)
+
+	//! Close the key.
 	void Close();
 
-	CString QueryString(const char* pszName, const char* pszDefault) const;
+	//
+	// Key value methods.
+	//
 
-protected:
+	//! Read the default value for the key.
+	CString ReadDefaultValue() const; // throw(RegistryException)
+
+	//! Read a named string value under the key.
+	CString ReadStringValue(const char* pszName, const char* pszDefault) const;
+
+	//! Write the default value for the key.
+	void WriteDefaultValue(const char* pszValue); // throw(RegistryException)
+
+	//! Write a named string value under the key.
+	void WriteStringValue(const char* pszName, const char* pszValue); // throw(RegistryException)
+
+	//
+	// Class methods.
+	//
+
+	//! Query if the key exists.
+	static bool Exists(HKEY hParentKey, const char* pszSubKey);
+
+	//! Delete the key.
+	static bool Delete(HKEY hParentKey, const char* pszSubKey);
+
+	//! Read the default value for a key.
+	static CString ReadKeyDefaultValue(HKEY hParentKey, const char* pszSubKey);
+
+	//! Write the default value for a key.
+	static void WriteKeyDefaultValue(HKEY hParentKey, const char* pszSubKey, const char* pszValue);
+
+	//! Write a named string value under a key.
+	static void WriteKeyStringValue(HKEY hParentKey, const char* pszSubKey, const char* pszName, const char* pszValue);
+
+private:
 	//
 	// Members.
 	//
-	HKEY m_hKey;
+	HKEY	m_hKey;		//!< The handle to the key.
+//	CString	m_strKey;	//!< The full path of the key.
+
+	// Disallow copying.
+	RegKey(const RegKey&);
+	RegKey& operator=(const RegKey&);
 };
 
-/******************************************************************************
-**
-** Implementation of inline functions.
-**
-*******************************************************************************
-*/
+////////////////////////////////////////////////////////////////////////////////
+//! Get the underlying handle.
 
-inline HKEY CRegKey::Handle() const
+inline HKEY RegKey::Handle() const
 {
 	return m_hKey;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Query if the key is open.
+
+inline bool RegKey::IsOpen() const
+{
+	return (m_hKey != NULL);
+}
+
+//namespace WCL
 }
 
 #endif //REGKEY_HPP
