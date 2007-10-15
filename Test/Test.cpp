9d6d6ce771a77328ebaec04a2437cb5588606955
Admin <admin@chrisoldwood.com>
2007-10-15 21:04:20 +0000

    Initial version.

---
+++ Test/Test.cpp
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+
+#include "stdafx.h"
+#include <WCL/SeTranslator.hpp>
+#include <WCL/StructuredException.hpp>
+
+////////////////////////////////////////////////////////////////////////////////
+//
+
+void TestStructuredException()
+{
+	WCL::SeTranslator oSeTranslator;
+
+	try
+	{
+		char* p = nullptr;
+
+		*p = '\0';
+	}
+	catch (const WCL::StructuredException& e)
+	{
+		std::tcout << e.what() << std::endl;
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+
+void TestString()
+{
+	std::tcout << CString::Fmt("%s", "Hello World") << std::endl;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+
+int _tmain(int /*argc*/, _TCHAR* /*argv*/[])
+{
+//	TestStructuredException();
+
+	TestString();
+	
+	return EXIT_SUCCESS;
+}
