6d680733b8b8fecfa2196eb305ac1fda65d574a3
Admin <admin@chrisoldwood.com>
2008-03-30 20:31:45 +0000

    Moved class from Date.hpp and Time.hpp

---
+++ DateSpan.hpp
@@ -0,0 +1,72 @@
+/******************************************************************************
+** (C) Chris Oldwood
+**
+** MODULE:		DATESPAN.HPP
+** COMPONENT:	Windows C++ Library.
+** DESCRIPTION:	The CDateSpan class declarations.
+**
+*******************************************************************************
+*/
+
+// Check for previous inclusion
+#ifndef WCL_DATESPAN_HPP
+#define WCL_DATESPAN_HPP
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+/******************************************************************************
+** 
+** This class is used to represent the distance between two dates in days.
+**
+*******************************************************************************
+*/
+
+class CDateSpan
+{
+public:
+	//
+	// Constructors/Destructor.
+	//
+	CDateSpan();
+	CDateSpan(days_t nDays);
+	
+	// Accessors.
+	int Days() const;
+
+protected:
+	//
+	// Members.
+	//
+	days_t	m_nSpan;
+
+	//
+	// Friends.
+	//
+	friend class CDate;
+};
+
+/******************************************************************************
+**
+** Implementation of CDateSpan inline functions.
+**
+*******************************************************************************
+*/
+
+inline CDateSpan::CDateSpan()
+	: m_nSpan(0)
+{
+}
+
+inline CDateSpan::CDateSpan(days_t nDays)
+	: m_nSpan(nDays)
+{
+}
+
+inline int CDateSpan::Days() const
+{
+	return m_nSpan;
+}
+
+#endif // WCL_DATESPAN_HPP
---
+++ TimeSpan.hpp
@@ -0,0 +1,90 @@
+/******************************************************************************
+** (C) Chris Oldwood
+**
+** MODULE:		TIMESPAN.HPP
+** COMPONENT:	Windows C++ Library.
+** DESCRIPTION:	The CTimeSpan class declarations.
+**
+*******************************************************************************
+*/
+
+// Check for previous inclusion
+#ifndef WCL_TIMESPAN_HPP
+#define WCL_TIMESPAN_HPP
+
+#if _MSC_VER > 1000
+#pragma once
+#endif
+
+/******************************************************************************
+** 
+** This class is used to represent a period between two times in seconds.
+**
+*******************************************************************************
+*/
+
+class CTimeSpan
+{
+public:
+	//
+	// Constructors/Destructor.
+	//
+	CTimeSpan();
+	CTimeSpan(seconds_t tSecs);
+	
+	// Accessors.
+	int Secs() const;
+	int Mins() const;
+	int Hours() const;
+	int Days() const;
+
+protected:
+	//
+	// Members.
+	//
+	seconds_t	m_tSpan;
+
+	//
+	// Friends.
+	//
+	friend class CTime;
+};
+
+/******************************************************************************
+**
+** Implementation of CTimeSpan inline functions.
+**
+*******************************************************************************
+*/
+
+inline CTimeSpan::CTimeSpan()
+	: m_tSpan(0)
+{
+}
+
+inline CTimeSpan::CTimeSpan(seconds_t tSecs)
+	: m_tSpan(tSecs)
+{
+}
+
+inline int CTimeSpan::Secs() const
+{
+	return static_cast<int>(m_tSpan);
+}
+
+inline int CTimeSpan::Mins() const
+{
+	return static_cast<int>(m_tSpan / SECS_PER_MIN);
+}
+
+inline int CTimeSpan::Hours() const
+{
+	return static_cast<int>(m_tSpan / SECS_PER_HOUR);
+}
+
+inline int CTimeSpan::Days() const
+{
+	return static_cast<int>(m_tSpan / SECS_PER_DAY);
+}
+
+#endif // WCL_TIMESPAN_HPP
