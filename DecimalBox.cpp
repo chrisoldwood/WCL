4ae33c9a5201a572b3a44150b353aa196a289d9b
Admin <admin@chrisoldwood.com>
2008-03-30 21:58:19 +0000

    Replaced use of std::logic_error with Core::BadLogicException.

--- DecimalBox.cpp
+++ DecimalBox.cpp
@@ -15,7 +15,7 @@
 #include <tchar.h>
 #include <Core/AnsiWide.hpp>
 #include <Core/StringUtils.hpp>
-#include <stdexcept>
+#include <Core/BadLogicException.hpp>
 
 /******************************************************************************
 ** Method:		Constructor.
@@ -117,7 +117,7 @@ void CDecimalBox::RealValue(double dValue)
 	ASSERT(nResult >= 0);
 
 	if (nResult < 0)
-		throw std::logic_error(T2A(Core::Fmt(TXT("Insufficient buffer size used in CDecimalBox::RealValue(). Result: %d"), nResult)));
+		throw Core::BadLogicException(Core::Fmt(TXT("Insufficient buffer size used in CDecimalBox::RealValue(). Result: %d"), nResult));
 
 	// Remove trailing zeroes?
 	if (m_nFlags & NO_TRAIL_ZEROES)
