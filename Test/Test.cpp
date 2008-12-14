895375e0ab41da5ac3dbdb70e68cbec20eda4024
Admin <admin@chrisoldwood.com>
2008-12-14 08:27:56 +0000

    Added support to allow single test cases to be specified on the command line. Added more tests.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -12,6 +12,7 @@
 
 extern void TestMisc();
 extern void TestString();
+extern void TestStringUtils();
 extern void TestDateAndTime();
 extern void TestIFacePtr();
 extern void TestComPtr();
@@ -19,23 +20,30 @@ extern void TestVerInfoReader();
 extern void TestIniFile();
 extern void TestRect();
 extern void TestFolderIterator();
+extern void TestIniFileCfgProvider();
+extern void TestRegistryCfgProvider();
+extern void TestAppConfig();
 
 ////////////////////////////////////////////////////////////////////////////////
 //! The entry point for the test harness.
 
-int _tmain(int /*argc*/, _TCHAR* /*argv*/[])
+int _tmain(int argc, _TCHAR* argv[])
 {
-	TEST_SUITE_BEGIN
+	TEST_SUITE_BEGIN(argc, argv)
 	{
-		TestMisc();
-		TestString();
-		TestDateAndTime();
-		TestIFacePtr();
-		TestComPtr();
-		TestVerInfoReader();
-		TestIniFile();
-		TestRect();
-		TestFolderIterator();
+		TEST_CASE(TestMisc);
+		TEST_CASE(TestString);
+		TEST_CASE(TestStringUtils);
+		TEST_CASE(TestDateAndTime);
+		TEST_CASE(TestIFacePtr);
+		TEST_CASE(TestComPtr);
+		TEST_CASE(TestVerInfoReader);
+		TEST_CASE(TestIniFile);
+		TEST_CASE(TestRect);
+		TEST_CASE(TestFolderIterator);
+		TEST_CASE(TestIniFileCfgProvider);
+		TEST_CASE(TestRegistryCfgProvider);
+		TEST_CASE(TestAppConfig);
 
 		Core::SetTestRunFinalStatus(true);
 	}
