4eb0582832932c30216932e99299da4498bc7f39
Admin <admin@chrisoldwood.com>
2009-08-01 23:34:26 +0000

    Updated as the tests are now self-registering.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -7,49 +7,7 @@
 #include <tchar.h>
 #include <Core/UnitTest.hpp>
 
-////////////////////////////////////////////////////////////////////////////////
-// The test group functions.
-
-extern void TestMisc();
-extern void TestString();
-extern void TestStringUtils();
-extern void TestPath();
-extern void TestDateAndTime();
-extern void TestIFacePtr();
-extern void TestComPtr();
-extern void TestComException();
-extern void TestVerInfoReader();
-extern void TestIniFile();
-extern void TestRect();
-extern void TestFolderIterator();
-extern void TestIniFileCfgProvider();
-extern void TestRegistryCfgProvider();
-extern void TestAppConfig();
-
-////////////////////////////////////////////////////////////////////////////////
-//! The entry point for the test harness.
-
 int _tmain(int argc, _TCHAR* argv[])
 {
-	TEST_SUITE_BEGIN(argc, argv)
-	{
-		TEST_CASE(TestMisc);
-		TEST_CASE(TestString);
-		TEST_CASE(TestStringUtils);
-		TEST_CASE(TestPath);
-		TEST_CASE(TestDateAndTime);
-		TEST_CASE(TestIFacePtr);
-		TEST_CASE(TestComPtr);
-		TEST_CASE(TestComException);
-		TEST_CASE(TestVerInfoReader);
-		TEST_CASE(TestIniFile);
-		TEST_CASE(TestRect);
-		TEST_CASE(TestFolderIterator);
-		TEST_CASE(TestIniFileCfgProvider);
-		TEST_CASE(TestRegistryCfgProvider);
-		TEST_CASE(TestAppConfig);
-
-		Core::setTestRunFinalStatus(true);
-	}
-	TEST_SUITE_END
+	TEST_SUITE_MAIN(argc, argv);
 }
