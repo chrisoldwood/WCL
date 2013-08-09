0f4bd547074e205dc5569de2428796b8a27e56aa
Admin <admin@chrisoldwood.com>
2013-08-09 23:55:00 +0000

    Rewrote tests to stop the compiler seeing the dead code in the release build.

--- Test/NullCmdControllerTests.cpp
+++ Test/NullCmdControllerTests.cpp
@@ -7,27 +7,30 @@
 #include <Core/UnitTest.hpp>
 #include <WCL/NullCmdController.hpp>
 #include <WCL/DC.hpp>
+#include <Core/UniquePtr.hpp>
 
 TEST_SET(NullCmdController)
 {
 
 TEST_CASE("All methods should throw a not implemented exception")
 {
+	typedef Core::UniquePtr<WCL::ICmdController> ICmdControllerPtr;
+
 	const uint TEST_COMMAND_ID = 99;
 
-	WCL::NullCmdController controller;
+	ICmdControllerPtr controller(new WCL::NullCmdController);
 
-	TEST_THROWS(controller.Execute(TEST_COMMAND_ID));
+	TEST_THROWS(controller->Execute(TEST_COMMAND_ID));
 
-	TEST_THROWS(controller.UpdateUI());
+	TEST_THROWS(controller->UpdateUI());
 
 	CDC& dc = *((CDC*)nullptr); // Shouldn't be invoked.
 
-	TEST_THROWS(controller.DrawCmd(TEST_COMMAND_ID, dc, CRect(), true));
+	TEST_THROWS(controller->DrawCmd(TEST_COMMAND_ID, dc, CRect(), true));
 
-	TEST_THROWS(controller.CmdHintStr(TEST_COMMAND_ID));
+	TEST_THROWS(controller->CmdHintStr(TEST_COMMAND_ID));
 
-	TEST_THROWS(controller.CmdToolTipStr(TEST_COMMAND_ID));
+	TEST_THROWS(controller->CmdToolTipStr(TEST_COMMAND_ID));
 }
 TEST_CASE_END
 
